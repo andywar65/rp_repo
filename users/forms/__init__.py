3555419fe40286f4f5e27e7417da12bab2a1487d
andywar65 <andy.war1965@gmail.com>
2020-04-17 21:30:02 +0200

    moved some forms in different files

--- users/forms/__init__.py
+++ users/forms/__init__.py
@@ -1,3 +1,4 @@
 from .back_forms import *
 from .front_forms import *
 from .contact_forms import *
+from .login_forms import *
--- users/forms/front_forms.py
+++ users/forms/front_forms.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from django import forms
 from django.contrib.auth import (password_validation, )
-from django.contrib.auth.forms import (AuthenticationForm, UsernameField,
-    PasswordResetForm, SetPasswordForm)
+from django.contrib.auth.forms import UsernameField
 from django.forms import ModelForm
 from django.forms.widgets import SelectDateWidget, CheckboxSelectMultiple
 from captcha.fields import ReCaptchaField
@@ -33,56 +32,6 @@ class ProfileAddChildForm(ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', )
-
-class FrontAuthenticationForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True,
-        }))
-    password = forms.CharField(
-        strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-            }),
-    )
-
-    def clean(self):
-        cd = super().clean()
-        try:
-            username = cd.get('username')
-            user = User.objects.get(username = username)
-            if user.profile.parent:
-                self.add_error(None, forms.ValidationError(
-                    """I minori non possono effettuare il login autonomamente!
-                    Il loro account è gestito dai genitori.""",
-                    code='minor_no_login',
-                ))
-        except:
-            pass
-
-class FrontPasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(
-        max_length=254, label='Email di registrazione',
-        widget=forms.EmailInput(attrs={'autocomplete': 'email',
-            })
-    )
-
-class FrontSetPasswordForm(SetPasswordForm):
-    new_password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
-            }),
-        strip=False, label='Nuova password',
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    new_password2 = forms.CharField(
-        strip=False, label='Ripeti la nuova password',
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
-            }),
-    )
-
-class FrontPasswordChangeForm(FrontSetPasswordForm):
-    old_password = forms.CharField(
-        strip=False, label='Vecchia password',
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-            'autofocus': True, }),
-    )
 
 class ProfileChangeForm(forms.Form):
     avatar = forms.FileField( required = False, widget = SmallClearableFileInput())
