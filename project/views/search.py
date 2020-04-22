115a364ae6a97dab8e3aeba2b9e978f63519ea12
andywar65 <andy.war1965@gmail.com>
2020-04-23 00:11:28 +0200

    added events in search

--- project/views/search.py
+++ project/views/search.py
@@ -17,9 +17,10 @@ def search_results(request):
         #search in uploads
         v = SearchVector('body')
         uploads = UserUpload.objects.annotate(rank=SearchRank(v, q))
-        uploads = uploads.filter(rank__gt=0.01).values_list('post_id',
+        #search in articles by upload
+        art_uploads = uploads.filter(rank__gt=0.01).values_list('post_id',
             flat = True)
-        article_uploads = Article.objects.filter(id__in = uploads)
+        article_uploads = Article.objects.filter(id__in = art_uploads)
         if article_uploads:
             success = True
         #search in articles
@@ -28,6 +29,19 @@ def search_results(request):
         articles = articles.filter(rank__gt=0.01)
         if articles:
             articles = articles.order_by('-rank')
+            success = True
+        #search in events by upload
+        evt_uploads = uploads.filter(rank__gt=0.01).values_list('event_id',
+            flat = True)
+        event_uploads = Event.objects.filter(id__in = evt_uploads)
+        if event_uploads:
+            success = True
+        #search in events
+        v = SearchVector('title', 'intro', 'stream_search')
+        events = Event.objects.annotate(rank=SearchRank(v, q))
+        events = events.filter(rank__gt=0.01)
+        if events:
+            events = events.order_by('-rank')
             success = True
         #search in pages
         #v = SearchVector('title', 'intro', 'stream_rendered')
@@ -38,6 +52,7 @@ def search_results(request):
             success = True
         return render(request, 'search_results.html',
             {'search': request.GET['q'], 'all_uploads': article_uploads,
-            'all_blogs': articles, 'pages': pages, 'success': success})
+            'all_blogs': articles, 'all_events': events,
+            'evt_uploads': event_uploads, 'pages': pages, 'success': success})
     else:
         return render(request, 'search_results.html', {'success': success, })
