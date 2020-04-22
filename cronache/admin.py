f16d361768ba2365950bbdf59592f2788c027978
andywar65 <andy.war1965@gmail.com>
2020-04-22 20:39:55 +0200

    deleted event upgrade inline

--- cronache/admin.py
+++ cronache/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.conf import settings
-from users.models import Member
-from .models import ( Location, Event, EventUpgrade, )
+from users.models import Profile
+from .models import ( Location, Event, )
 from .forms import EventForm
-from rpnew_prog.utils import send_rp_mail
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
@@ -11,18 +10,12 @@ class LocationAdmin(admin.ModelAdmin):
     readonly_fields = ('slug', )
     search_fields = ('title', 'address')
 
-class EventUpgradeInline(admin.TabularInline):
-    model = EventUpgrade
-    fields = ('title', 'date', 'body', )
-    extra = 0
-
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'location', 'notice')
     inlines = [ UserUploadInline ]
     search_fields = ('title', 'date', 'intro', )
     form = EventForm
-    actions = ['send_notice', ]
     autocomplete_fields = ['location', ]
 
     fieldsets = (
@@ -53,24 +46,3 @@ class EventAdmin(admin.ModelAdmin):
             'fields': ('manager', 'tags', 'notice')
         }),
     )
-
-    def send_notice(self, request, queryset):
-        for event in queryset:
-            message = event.title + '\n'
-            #upgrades = EventUpgrade.objects.filter(event_id=event.id)
-            #if upgrades:
-                #upgrade = upgrades[0]
-                #message += upgrade.body + '\n'
-            message += event.intro + '\n'
-            url = settings.BASE_URL + event.get_path()
-            message += 'Fai click su questo link: ' + url + '\n'
-            recipients = Member.objects.filter(parent = None,
-                user__is_active = True, no_spam = True, )
-            mailto = []
-            for recipient in recipients:
-                mailto.append(recipient.email)
-            subject = 'Nuovo appuntamento / aggiornamento RP'
-            send_rp_mail(subject, message, mailto)
-            event.notice = 'DONE'
-            event.save()
-    send_notice.short_description = 'Invia notifiche'
