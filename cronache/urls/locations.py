1ddd0bdc684df0757e93a3ad5f2268755b3729d5
andywar65 <andy.war1965@gmail.com>
2020-05-27 23:29:24 +0200

    added locations namespace

--- cronache/urls/locations.py
+++ cronache/urls/locations.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from cronache.views import ListLocation, DetailLocation
 
+app_name = 'locations'
+
 urlpatterns = [
     path('', ListLocation.as_view(), name='locations'),
     path('<slug>/', DetailLocation.as_view(), name='location'),
