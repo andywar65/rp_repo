702b2308acd48f7137d00ad5fe0e6aeb9319b4b9
andywar65 <andy.war1965@gmail.com>
2020-05-30 00:13:07 +0200

    direzione model coverage at 100%

--- direzione/tests.py
+++ direzione/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from cronache.models import Location
+from .models import Convention, Society
+
+class ConventionModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        location = Location.objects.create(title='Place', address='Nowhere St.',
+            gmap_embed = 'http')
+        Convention.objects.create(title='Doctor Who', location=location)
+        Society.objects.create(title='Rif Pod', denomination='ASD')
+
+    def test_convention_model_str_method(self):
+        convention = Convention.objects.get(slug='doctor-who')
+        self.assertEqual(convention.__str__(), 'Doctor Who')
+
+    def test_convention_model_get_location(self):
+        location = Location.objects.get(slug='place')
+        convention = Convention.objects.get(slug='doctor-who')
+        self.assertEqual(convention.get_location(), location)
+
+    def test_convention_model_get_path(self):
+        convention = Convention.objects.get(slug='doctor-who')
+        self.assertEqual(convention.get_path(), '/convenzioni/doctor-who')
+
+    def test_society_model_str_method(self):
+        society = Society.objects.get(title='Rif Pod')
+        self.assertEqual(society.__str__(), 'Rif Pod')
--- direzione/views.py
+++ direzione/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Convention
 
