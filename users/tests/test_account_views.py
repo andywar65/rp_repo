e10732f786a19c8f8c0a3c2b07566135fc9db35b
andywar65 <andy.war1965@gmail.com>
2020-06-08 17:07:16 +0200

    testing profile add child view get method

---
+++ users/tests/test_account_views.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from users.models import User
+
+class UserViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        user0 = User.objects.create_user(username='user_0',
+            password='P4s5W0r6', email='me@existing.com')
+        profile = user0.profile
+        profile.sector = '0-NO'
+        profile.is_trusted = True
+        profile.save()
+        untrusty = User.objects.create_user(username='untrusty',
+            password='P4s5W0r6', email='me@existing.com')
+        profile = user0.profile
+        profile.sector = '3-FI'
+        profile.is_trusted = False
+        profile.save()
+
+    def test_profile_add_child_view_404_sector_0(self):
+        self.client.post('/accounts/login/', {'username':'user_0',
+            'password':'P4s5W0r6'})
+        response = self.client.get(reverse('profile_add_child'))
+        self.assertEqual(response.status_code, 404 )
+
+    def test_profile_add_child_view_404_untrusted(self):
+        self.client.post('/accounts/login/', {'username':'untrusty',
+            'password':'P4s5W0r6'})
+        response = self.client.get(reverse('profile_add_child'))
+        self.assertEqual(response.status_code, 404 )
