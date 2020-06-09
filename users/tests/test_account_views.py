e5351675d018b7bad27d16f8f5ae7b6fe73ea93a
andywar65 <andy.war1965@gmail.com>
2020-06-09 13:10:00 +0200

    added profile change registry view tests

--- users/tests/test_account_views.py
+++ users/tests/test_account_views.py
@@ -245,3 +245,81 @@ class AccountViewTest(TestCase):
             'email': parent.email})
         self.assertRedirects(response,
             f'/accounts/profile/?submitted={child.first_name}%20{child.last_name}' )
+
+    #testing profile change registry view
+
+    def test_profile_change_registry_not_logged(self):
+        parent = User.objects.get(username='trustyparent')
+        child = User.objects.get(username='trustychild')
+        response = self.client.get(f'/accounts/profile/{child.id}/change/registry/?parent={parent.id}')
+        self.assertEqual(response.status_code, 302 )
+
+    def test_profile_change_registry_not_logged_redirects(self):
+        parent = User.objects.get(username='trustyparent')
+        child = User.objects.get(username='trustychild')
+        response = self.client.get(f'/accounts/profile/{child.id}/change/registry/?parent={parent.id}')
+        self.assertRedirects(response,
+            f'/accounts/login/?next=/accounts/profile/{child.id}/change/registry/?parent={parent.id}' )
+
+    def test_profile_change_registry_404_wrong_parent(self):
+        parent = User.objects.get(username='user_2')
+        child = User.objects.get(username='trustychild')
+        self.client.post('/accounts/login/', {'username':'user_2',
+            'password':'P4s5W0r6'})
+        response = self.client.get(f'/accounts/profile/{child.id}/change/registry/?parent={parent.id}')
+        self.assertEqual(response.status_code, 404 )
+
+    def test_profile_change_registry_404_wrong_id(self):
+        parent = User.objects.get(username='user_2')
+        self.client.post('/accounts/login/', {'username':'user_2',
+            'password':'P4s5W0r6'})
+        response = self.client.get('/accounts/profile/404/change/registry/')
+        self.assertEqual(response.status_code, 404 )
+
+    def test_profile_change_registry_404_wrong_child(self):
+        parent = User.objects.get(username='trustyparent')
+        self.client.post('/accounts/login/', {'username':'trustyparent',
+            'password':'P4s5W0r6'})
+        response = self.client.get(f'/accounts/profile/404/change/registry/?parent={parent.id}')
+        self.assertEqual(response.status_code, 404 )
+
+    def test_profile_change_registry_status_code(self):
+        parent = User.objects.get(username='trustyparent')
+        child = User.objects.get(username='trustychild')
+        self.client.post('/accounts/login/', {'username':'trustyparent',
+            'password':'P4s5W0r6'})
+        response = self.client.get(f'/accounts/profile/{child.id}/change/registry/?parent={parent.id}')
+        self.assertEqual(response.status_code, 200 )
+
+    def test_profile_change_registry_template(self):
+        parent = User.objects.get(username='trustyparent')
+        child = User.objects.get(username='trustychild')
+        self.client.post('/accounts/login/', {'username':'trustyparent',
+            'password':'P4s5W0r6'})
+        response = self.client.get(f'/accounts/profile/{child.id}/change/registry/?parent={parent.id}')
+        self.assertTemplateUsed(response, 'users/profile_change_registry.html' )
+
+    def test_profile_change_registry_post_status_code(self):
+        parent = User.objects.get(username='trustyparent')
+        child = User.objects.get(username='trustychild')
+        self.client.post('/accounts/login/', {'username':'trustyparent',
+            'password':'P4s5W0r6'})
+        response = self.client.post(f'/accounts/profile/{child.id}/change/registry/?parent={parent.id}',
+            {'gender': 'F', 'date_of_birth_day': '4',
+            'date_of_birth_month': '6', 'date_of_birth_year': '1999',
+            'place_of_birth': 'Roma', 'nationality': 'Italiana',
+            'fiscal_code': 'GRRNNA99H44H501X'})
+        self.assertEqual(response.status_code, 302 )
+
+    def test_profile_change_registry_post_redirects(self):
+        parent = User.objects.get(username='trustyparent')
+        child = User.objects.get(username='trustychild')
+        self.client.post('/accounts/login/', {'username':'trustyparent',
+            'password':'P4s5W0r6'})
+        response = self.client.post(f'/accounts/profile/{child.id}/change/registry/?parent={parent.id}',
+            {'gender': 'F', 'date_of_birth_day': '4',
+            'date_of_birth_month': '6', 'date_of_birth_year': '1999',
+            'place_of_birth': 'Roma', 'nationality': 'Italiana',
+            'fiscal_code': 'GRRNNA99H44H501X'})
+        self.assertRedirects(response,
+            f'/accounts/profile/?submitted={child.first_name}%20{child.last_name}' )
